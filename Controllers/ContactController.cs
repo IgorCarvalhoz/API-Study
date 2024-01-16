e4eb2fcd85993c8ce334ca61e24a3d4d9a4549c8
Igor Carvalho <igor.carvalho.ti@gmail.com>
2024-01-16 15:25:34 -0300

    finishing the crud functions

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -23,11 +23,11 @@ namespace API_Study.Controllers
             _context.SaveChanges();
             return Ok(contact);
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult Delete(Contact contact){ //This method createthe Delete action in schedule Table, here we can remove the infos in DB.
             _context.Remove(contact);
             _context.SaveChanges();
-            return Ok(contact);
+            return NoContent();
         }
         [HttpGet("{id}")]
         public IActionResult GetID(int id){
@@ -37,14 +37,28 @@ namespace API_Study.Controllers
             }
             return Ok(contato);
         }
-        
+        [HttpGet("GetByName")]
+        public IActionResult GetByName(string name){
+            var contacts = _context.Contacts.Where(x =>  x.Name.Contains(name));
+            return Ok(contacts);
+        }
+
+
+
+
+        [HttpPut ("{id}")]
         public IActionResult Update(int id, Contact contact){
-            var contactDBA = _context.Contacts.Find(id);
-            if (contactDBA == null){
+            var contactDBA = _context.Contacts.Find(id); //Get DBA infos
+            if (contactDBA == null){ //Makes the verification if the id found
                 return NotFound();
             }
-            contactDBA.Name = contact.Name;
-            return Ok(contact);
-        }
+            contactDBA.Name = contact.Name; //Makes the modifications in the DBA receiving the resiquitation parameters "Contact contact"
+            contactDBA.Phone = contact.Phone;
+            contactDBA.Active = contact.Active;
+
+            _context.Contacts.Update(contactDBA);//Makes the update
+            _context.SaveChanges();//Saves the changes
+            return Ok(contactDBA);//Get the actualized contact
+        } 
     }
 }
